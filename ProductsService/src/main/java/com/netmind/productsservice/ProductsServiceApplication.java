c6c42dcd49bdcd6fe157510d8df6970177185b16
ricardonetmind <ricardoag@expert.netmind.es>
2022-10-16 17:23:51 +0200

    update

--- ProductsService/src/main/java/com/netmind/productsservice/ProductsServiceApplication.java
+++ ProductsService/src/main/java/com/netmind/productsservice/ProductsServiceApplication.java
@@ -4,10 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ProductsServiceApplication {
@@ -16,14 +12,5 @@ public class ProductsServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductsServiceApplication.class, args);
     }
-
-    /*@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/products").allowedOrigins("*");
-            }
-        };
-    }*/
 
 }
