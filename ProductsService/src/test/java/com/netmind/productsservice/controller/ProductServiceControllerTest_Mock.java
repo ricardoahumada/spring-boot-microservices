587583ad6d8246cb26292d390af1213f82b998bd
ricardonetmind <ricardoag@expert.netmind.es>
2022-10-16 09:31:33 +0200

    update

--- ProductsService/src/test/java/com/netmind/productsservice/controller/ProductServiceControllerTest_Mock.java
+++ ProductsService/src/test/java/com/netmind/productsservice/controller/ProductServiceControllerTest_Mock.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -22,7 +21,7 @@ import static org.hamcrest.Matchers.is;
         classes = ProductsServiceApplication.class)
 @AutoConfigureMockMvc
 //@TestPropertySource( locations = "classpath:application-integrationtest.properties")
-class ProductServiceControllerTest {
+class ProductServiceControllerTest_Mock {
     @Autowired
     private MockMvc mvc;
 
