4539013e3d8d78b1666c92f991e45a52cb8313bf
ricardonetmind <ricardoag@expert.netmind.es>
2022-10-24 21:01:47 +0200

    update

---
+++ microservices-own/orders-service/src/main/java/com/netmind/microservices/ordersservice/model/ProductBean.java
@@ -0,0 +1,15 @@
+package com.netmind.microservices.ordersservice.model;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ProductBean {
+    private Long id;
+    private String name;
+    private String serial;
+    private Integer price;
+}
