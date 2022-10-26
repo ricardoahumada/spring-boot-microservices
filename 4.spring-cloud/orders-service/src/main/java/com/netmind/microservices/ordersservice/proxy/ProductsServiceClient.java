e39e0906750427be2a283b11c0deef94554cc4a5
ricardonetmind <ricardoag@expert.netmind.es>
2022-10-26 13:59:57 +0200

    update

--- 4.spring-cloud/orders-service/src/main/java/com/netmind/microservices/ordersservice/proxy/ProductsServiceClient.java
+++ 4.spring-cloud/orders-service/src/main/java/com/netmind/microservices/ordersservice/proxy/ProductsServiceClient.java
@@ -1,8 +1,8 @@
 package com.netmind.microservices.ordersservice.proxy;
 
-//import com.netmind.microservices.ordersservice.config.LoadBalancerConfiguration;
+import com.netmind.microservices.ordersservice.config.LoadBalancerConfiguration;
 import com.netmind.microservices.ordersservice.model.ProductBean;
-//import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
