9d0a749cbe4f6c597a44979f70fc72d3b176065d
ricardonetmind <ricardoag@expert.netmind.es>
2022-10-28 10:40:04 +0200

    update

--- 4.spring-cloud/gateway-server/src/main/java/com/netmind/microservices/gatewayserver/GatewayServerApplication.java
+++ 4.spring-cloud/gateway-server/src/main/java/com/netmind/microservices/gatewayserver/GatewayServerApplication.java
@@ -3,11 +3,11 @@ package com.netmind.microservices.gatewayserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableWebFluxSecurity
+//@EnableWebFluxSecurity
 public class GatewayServerApplication {
 
     public static void main(String[] args) {
