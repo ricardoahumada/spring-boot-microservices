9f6e3d87ab8c41854d4e272690bb8ea21c980a5e
ricardonetmind <ricardoag@expert.netmind.es>
2022-10-28 23:33:45 +0200

    update

--- kubernetize/gateway-server/src/main/java/com/netmind/microservices/gatewayserver/GatewayServerApplication.java
+++ kubernetize/gateway-server/src/main/java/com/netmind/microservices/gatewayserver/GatewayServerApplication.java
@@ -2,10 +2,8 @@ package com.netmind.microservices.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
