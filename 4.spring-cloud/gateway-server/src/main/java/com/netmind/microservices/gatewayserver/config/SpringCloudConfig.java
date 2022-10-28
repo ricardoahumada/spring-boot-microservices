0b4cc13a125c6a2d6c77c7e8aecffbca308ff1d7
ricardonetmind <ricardoag@expert.netmind.es>
2022-10-28 10:29:22 +0200

    update

--- 4.spring-cloud/gateway-server/src/main/java/com/netmind/microservices/gatewayserver/config/SpringCloudConfig.java
+++ 4.spring-cloud/gateway-server/src/main/java/com/netmind/microservices/gatewayserver/config/SpringCloudConfig.java
@@ -1,17 +1,15 @@
 package com.netmind.microservices.gatewayserver.config;
 
-/*
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
-*/
 
-// @Configuration
+ @Configuration
 public class SpringCloudConfig {
-    /*@Bean
+    @Bean
 	public GlobalFilter globalFilter() {
 		return (exchange, chain) -> {
 			System.out.println("First Global filter");
@@ -19,21 +17,21 @@ public class SpringCloudConfig {
 				System.out.println("Second Global filter");
 			}));
 		};
-	}*/
+	}
 
-    /*@Bean
+    @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/orders/**")
                         .uri("lb://orders-service/"))
                 .route(r -> r.path("/products/**")
-                        // .filters(f ->
-                        //         f.addRequestHeader("added-request-header", "added-request-header-value")
-                        //         .addResponseHeader("added-response-header", "added-response-header-value")
-                        // )
+                         .filters(f ->
+                                 f.addRequestHeader("added-request-header", "added-request-header-value")
+                                 .addResponseHeader("added-response-header", "added-response-header-value")
+                         )
                         .uri("lb://products-service/"))
 
                 .build();
-    }*/
+    }
 
 }
