e928ae35d47d86ba9b7e26dfb7e054a874deff99
ricardonetmind <ricardoag@expert.netmind.es>
2023-09-14 17:57:07 +0200

    update

--- 03.security/oauth/oauth2-resource-server/src/main/java/com/netmind/oauth2resourceserver/config/ResourceServerConfig.java
+++ 03.security/oauth/oauth2-resource-server/src/main/java/com/netmind/oauth2resourceserver/config/ResourceServerConfig.java
@@ -1,26 +1,44 @@
 package com.netmind.oauth2resourceserver.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
 @Configuration
-@EnableWebSecurity
 @EnableMethodSecurity
 public class ResourceServerConfig {
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuer;
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-
-        http.authorizeRequests()
-                .requestMatchers("/products/**").authenticated()
-                .requestMatchers("/products/**").hasAuthority("SCOPE_products.read")
-                .and()
-                .oauth2ResourceServer()
-                .jwt();
+        http
+                .authorizeHttpRequests(
+                        authorize -> authorize
+                                .requestMatchers(antMatcher("/docs/**")).permitAll()
+                                .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oAuth2 -> oAuth2
+                        .jwt(it -> it.decoder(JwtDecoders.fromIssuerLocation(issuer))));
         return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("");
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
     }
 }
