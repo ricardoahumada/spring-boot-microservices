084a0df52e56b97f7a1ad44333a30da606a1793a
ricardonetmind <ricardoag@expert.netmind.es>
2023-09-14 16:34:18 +0200

    update

--- 03.security/oauth/oauth2-resource-server/src/main/java/com/netmind/oauth2resourceserver/config/ResourceServerConfig.java
+++ 03.security/oauth/oauth2-resource-server/src/main/java/com/netmind/oauth2resourceserver/config/ResourceServerConfig.java
@@ -1,17 +1,23 @@
 package com.netmind.oauth2resourceserver.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class ResourceServerConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+
         http.authorizeRequests()
-                .mvcMatchers("/products/**").authenticated()
-                .mvcMatchers("/products/**").hasAuthority("SCOPE_products.read")
+                .requestMatchers("/products/**").authenticated()
+                .requestMatchers("/products/**").hasAuthority("SCOPE_products.read")
                 .and()
                 .oauth2ResourceServer()
                 .jwt();
