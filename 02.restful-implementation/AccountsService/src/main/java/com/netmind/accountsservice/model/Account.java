e58881491111803282166fa6d38f7c34593751ef
ricardonetmind <ricardoag@expert.netmind.es>
2023-10-02 22:24:15 +0200

    accounts service open api

--- 02.restful-implementation/AccountsService/src/main/java/com/netmind/accountsservice/model/Account.java
+++ 02.restful-implementation/AccountsService/src/main/java/com/netmind/accountsservice/model/Account.java
@@ -19,28 +19,33 @@ import java.util.Date;
 @AllArgsConstructor
 @Entity
 @Table(name = "accounts")
-@Schema(name = "account", description = "Modelo cuenta")
+@Schema(name = "account", description = "Account model")
 @XmlRootElement
 public class Account {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Min(0)
+    @Schema(name = "Account ID", example = "1", required = false)
     private Long id;
 
     @NotBlank(message = "Debe tener valor")
     @NotNull
     @Size(min = 3, max = 50)
+    @Schema(name = "Account type", example = "Personal", required = true)
     private String type;
 
     @DateTimeFormat
     @NotNull
+    @Schema(name = "Opening date", example = "2023-10-01", required = true)
     Date openingDate;
 
     @Min(0)
+    @Schema(name = "Current balance", example = "10245.67", required = true)
     private int balance;
 
     @Min(1)
+    @Schema(name = "Customer id", example = "34", required = true)
     private Long ownerId;
 
     @Transient
