27d43c6ed8b3c7f0d17f8e32ac4d930ea8396134
ricardo ahumada <ricardo@enmotionvalue.com>
2023-10-04 17:14:49 +0200

    update

--- 02.restful-implementation/ProductsService/src/test/java/com/netmind/productsservice/controller/ProductServiceControllerTest_MockMvc.java
+++ 02.restful-implementation/ProductsService/src/test/java/com/netmind/productsservice/controller/ProductServiceControllerTest_MockMvc.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netmind.productsservice.ProductsServiceApplication;
 import com.netmind.productsservice.model.Product;
 import com.netmind.productsservice.persistence.ProductsRepository;
+import com.netmind.productsservice.util.JsonUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,14 +15,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
-import static org.hamcrest.Matchers.is;
 
 
 // TODO: uncomment and implement methods
@@ -51,7 +50,17 @@ class ProductServiceControllerTest_MockMvc {
 
     @Test
     void givenProducts_whenValidCreateProduct_thenIsCreatedAndHaveId() throws Exception {
+        Product nuevoProd = new Product(null, "Nuevo Prod", "123-122-1223");
 
+        mvc.perform(post("/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtil.asJsonString(nuevoProd))
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isCreated())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.id", is(greaterThanOrEqualTo(1))));
     }
 
 }
