2859b79153edcff5cc55139d62b05a0fb60e2bde
ricardo ahumada <ricardo@enmotionvalue.com>
2023-10-04 16:46:29 +0200

    update

--- 02.restful-implementation/ProductsService/src/test/java/com/netmind/productsservice/controller/ProductServiceControllerTest_MockMvc.java
+++ 02.restful-implementation/ProductsService/src/test/java/com/netmind/productsservice/controller/ProductServiceControllerTest_MockMvc.java
@@ -9,10 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.hamcrest.Matchers.hasItem;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -23,21 +25,33 @@ import static org.hamcrest.Matchers.is;
 
 
 // TODO: uncomment and implement methods
-// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-// @AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+//@Sql(value = "classpath:test.sql")
 // @TestPropertySource( locations = "classpath:application-integrationtest.properties")
 class ProductServiceControllerTest_MockMvc {
+    @Autowired
+    private MockMvc mvc;
     @Autowired
     private ProductsRepository repository;
 
     @Test
     public void givenProducts_whenGetProducts_thenStatus200() throws Exception {
+        Product nuevoProd = new Product(null, "Nuevo Prod", "123-122-1223");
+        repository.save(nuevoProd);
 
-    
-}
+        mvc.perform(get("/products").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].name", is("Nuevo Prod")));
+//                .andExpect(jsonPath("$[*].name", hasItem("Nuevo Prod"))); // con @sql
+
+    }
+
     @Test
     void givenProducts_whenValidCreateProduct_thenIsCreatedAndHaveId() throws Exception {
 
     }
-    
+
 }
