e6dd201a69df8eb35845ef964fde243539af7a99
ricardo ahumada <ricardo@enmotionvalue.com>
2023-10-06 12:28:25 +0200

    update

--- 03.security/basic/Spring-Security-Authentication-Flow/src/main/java/com/netmind/demo/security/SecurityConfig.java
+++ 03.security/basic/Spring-Security-Authentication-Flow/src/main/java/com/netmind/demo/security/SecurityConfig.java
@@ -6,19 +6,20 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.Customizer;
 
 
-// @Configuration // TODO: UNCOMMENT
+@Configuration // TODO: UNCOMMENT
 public class SecurityConfig {
 
     // TODO: UNCOMMENT 
-    /*@Bean
+    @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
         return http.getSharedObject(AuthenticationManagerBuilder.class)
                 .build();
-    }*/
+    }
 
     // TODO: UNCOMMENT 
     /*@Bean   
@@ -29,17 +30,21 @@ public class SecurityConfig {
     }*/
 
     // TODO: UNCOMMENT 
-    /*@Bean
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http
                 .authorizeHttpRequests((request) -> request
+//                        .requestMatchers("/**").denyAll()
+//                        .requestMatchers("/resource").permitAll()
+                        .requestMatchers("/auth/login").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults());
+                /*.httpBasic(Customizer.withDefaults())*/;
+
         return http.build();
-    }*/
+    }
 
 }
