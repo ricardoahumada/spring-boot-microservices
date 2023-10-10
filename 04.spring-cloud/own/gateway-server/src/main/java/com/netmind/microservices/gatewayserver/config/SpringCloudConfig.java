a6b42801547287e5891b37181ad41269de1987b8
ricardonetmind <ricardoag@expert.netmind.es>
2023-10-10 18:23:05 +0200

    update

--- 04.spring-cloud/own/gateway-server/src/main/java/com/netmind/microservices/gatewayserver/config/SpringCloudConfig.java
+++ 04.spring-cloud/own/gateway-server/src/main/java/com/netmind/microservices/gatewayserver/config/SpringCloudConfig.java
@@ -8,14 +8,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import reactor.core.publisher.Mono;
 
-@Configuration
+// TODO: uncomment for routes
+// @Configuration
 public class SpringCloudConfig {
-    @Bean
+    // TODO: uncomment for global filter
+    // @Bean
     public GlobalFilter globalFilter() {
         return (exchange, chain) -> {
-            System.out.println("First Global filter");
+            System.out.println("Global filter 1");
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                System.out.println("Second Global filter");
+                System.out.println("Global filter 2");
             }));
         };
     }
@@ -24,20 +26,22 @@ public class SpringCloudConfig {
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 // Open Api
-                .route(r -> r.path("/products-service/v3/api-docs")
+                // TODO: uncomment for api docs
+                /*.route(r -> r.path("/products-service/v3/api-docs")
                         .filters(f->f.rewritePath("/products-service/(?<path>.*)","/${path}"))
                         .uri("lb://products-service"))
                 .route(r -> r.path("/orders-service/v3/api-docs")
                         .filters(f->f.rewritePath("/orders-service/v3/api-docs","/v3/api-docs"))
-                        .uri("lb://orders-service"))
+                        .uri("lb://orders-service"))*/
                 // Endpoints
-                .route(r -> r.path("/orders/**")
+                // TODO: uncomment for routes
+                /*.route(r -> r.path("/orders/**")
                         .uri("lb://orders-service/"))
                 .route(r -> r.path("/products/**")
                         .filters(f ->
                                 f.addRequestHeader("added-request-header", "added-request-header-value")
                                         .addResponseHeader("added-response-header", "added-response-header-value")
-                        ).uri("lb://products-service/"))
+                        ).uri("lb://products-service/"))*/
                 .build();
     }
 
