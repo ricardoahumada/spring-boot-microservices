0c8df1e4e1c762ec34f41b435db7d357a7965de3
ricardo ahumada <ricardo@enmotionvalue.com>
2023-10-10 09:26:00 +0200

    update

--- 03.security/oauth/AccountsService/src/test/java/com/netmind/accountsservice/controller/AccountControllerTest.java
+++ 03.security/oauth/AccountsService/src/test/java/com/netmind/accountsservice/controller/AccountControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
