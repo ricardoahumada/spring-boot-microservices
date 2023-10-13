52c58f3f664dc435217c3adc639b4f3ba02d3329
ricardoahumada <ricardo@enmotionvalue.com>
2023-10-13 17:15:55 +0200

    update k8

---
+++ 06.kubernetize/mvn-images-creator.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Creating config-server image..."
+cd ./config-server
+mvn spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/config-server:sb3
+cd ..
+
+echo "Creating gateway-server image..."
+cd ./gateway-server
+mvn spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/gateway-server:sb3
+cd ..
+
+echo "Creating products-service image..."
+cd ./products-service
+mvn spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/products-service:sb3
+cd ..
+
+echo "Creating orders-service image..."
+cd ./orders-service
+mvn spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/orders-service:sb3
+cd ..
+
+docker images
