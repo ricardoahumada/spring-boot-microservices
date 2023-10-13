f5762a4c55594e121d626930897bffdf87a90ea2
ricardonetmind <ricardoag@expert.netmind.es>
2023-10-13 18:01:29 +0200

    update k8

--- 06.kubernetize/mvn-images-creator.sh
+++ 06.kubernetize/mvn-images-creator.sh
@@ -2,22 +2,22 @@
 
 echo "Creating config-server image..."
 cd ./config-server
-mvn spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/config-server:sb3
+mvn spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/config-server:sb3.1
 cd ..
 
 echo "Creating gateway-server image..."
 cd ./gateway-server
-mvn spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/gateway-server:sb3
+mvn spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/gateway-server:sb3.1
 cd ..
 
 echo "Creating products-service image..."
 cd ./products-service
-mvn spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/products-service:sb3
+mvn spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/products-service:sb3.1
 cd ..
 
 echo "Creating orders-service image..."
 cd ./orders-service
-mvn spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/orders-service:sb3
+mvn spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/orders-service:sb3.1
 cd ..
 
 docker images
