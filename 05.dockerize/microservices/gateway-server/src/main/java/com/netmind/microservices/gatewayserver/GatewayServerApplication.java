bf8a7a2ad40a1682dec828929e4eab7a71b8f90e
ricardonetmind <ricardoag@expert.netmind.es>
2023-10-13 07:32:54 +0200

    update docker

--- 05.dockerize/microservices/gateway-server/src/main/java/com/netmind/microservices/gatewayserver/GatewayServerApplication.java
+++ 05.dockerize/microservices/gateway-server/src/main/java/com/netmind/microservices/gatewayserver/GatewayServerApplication.java
@@ -1,5 +1,7 @@
 package com.netmind.microservices.gatewayserver;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,6 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 //@EnableWebFluxSecurity
+@OpenAPIDefinition(info = @Info(title = "API Gateway", version = "1.0", description = "Documentation API Gateway v1.0"))
 public class GatewayServerApplication {
 
     public static void main(String[] args) {
