03d73fe1dbce4954c75250fbadf6000036912ad9
ricardonetmind <ricardoag@expert.netmind.es>
2023-10-13 16:00:55 +0200

    update docker

--- 05.dockerize/microservices/orders-service/src/main/java/com/netmind/microservices/ordersservice/proxy/ProductsServiceClient.java
+++ 05.dockerize/microservices/orders-service/src/main/java/com/netmind/microservices/ordersservice/proxy/ProductsServiceClient.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 //@FeignClient(name = "products-service", url = "${products-service.base-url}")
 @FeignClient(name = "products-service") //for eureka and load balancer
-//@LoadBalancerClient(name = "products-service", configuration= LoadBalancerConfiguration.class)
 public interface ProductsServiceClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "${products-service.path}")
---
+++ 05.dockerize/microservices/users-management-service/src/test/java/com/netmind/microservices/usermanagemeentservice/UsersManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netmind.microservices.usermanagemeentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
