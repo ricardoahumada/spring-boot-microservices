abcfb54c36ab3f59271f3202596bb35d3a920649
ricardoahumada <ricardo@enmotionvalue.com>
2023-10-14 18:29:52 +0200

    update k8

--- 06.kubernetize/mvn-images-creator.sh
+++ 06.kubernetize/mvn-images-creator.sh
@@ -2,27 +2,27 @@
 
 echo "Creating config-server image..."
 cd ./config-server
-mvn spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/config-server:sb3.1
+mvn clean spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/config-server:sb3.1
 cd ..
 
 echo "Creating gateway-server image..."
 cd ./gateway-server
-mvn spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/gateway-server:sb3.1
+mvn clean spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/gateway-server:sb3.1
 cd ..
 
 echo "Creating products-service image..."
 cd ./products-service
-mvn spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/products-service:sb3.1
+mvn clean spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/products-service:v3
 cd ..
 
 echo "Creating orders-service image..."
 cd ./orders-service
-mvn spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/orders-service:sb3.1
+mvn clean spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/orders-service:v3
 cd ..
 
 echo "Creating users-management-service image..."
 cd ./users-management-service
-mvn spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/users-management-service:sb3.1
+mvn clean spring-boot:build-image -Dspring-boot.build-image.imageName=ricardoahumada/users-management-service:sb3.1
 cd ..
 
 docker images | grep "sb3.1"
--- 06.kubernetize/push-images.sh
+++ 06.kubernetize/push-images.sh
@@ -7,10 +7,10 @@ echo "Pushing gateway-server image..."
 docker push ricardoahumada/gateway-server:sb3.1
 
 echo "Pushing products-service image..."
-docker push ricardoahumada/products-service:sb3.1
+docker push ricardoahumada/products-service:v3
 
 echo "Pushing orders-service image..."
-docker push ricardoahumada/orders-service:sb3.1
+docker push ricardoahumada/orders-service:v3
 
 echo "Pushing users-management-service image..."
 docker push ricardoahumada/users-management-service:sb3.1
