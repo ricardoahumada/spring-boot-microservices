769549f65e12cf18f6e9aa8fb8c8a6280bb47b8d
ricardonetmind <ricardoag@expert.netmind.es>
2023-10-19 08:50:40 +0200

    update

--- 06.kubernetize/accounts-service/src/main/java/com/netmind/accountsservice/proxy/CustomersServiceClient.java
+++ 06.kubernetize/accounts-service/src/main/java/com/netmind/accountsservice/proxy/CustomersServiceClient.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "customers-service")
+// @FeignClient(name = "customers-service")
+@FeignClient(value = "customersservice", url = "${customers-service.base-url}")
 public interface CustomersServiceClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "${customers-service.path}")
