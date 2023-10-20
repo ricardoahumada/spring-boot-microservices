626e31d9008aa47769ae88fd49a9dc74ef26da4b
ricardonetmind <ricardoag@expert.netmind.es>
2023-10-20 07:33:29 +0200

    update

--- 06.kubernetize/orders-service/src/main/java/com/netmind/microservices/ordersservice/OrdersServiceApplication.java
+++ 06.kubernetize/orders-service/src/main/java/com/netmind/microservices/ordersservice/OrdersServiceApplication.java
@@ -1,5 +1,6 @@
 package com.netmind.microservices.ordersservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,7 +10,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableDiscoveryClient
 public class OrdersServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OrdersServiceApplication.class, args);
     }
