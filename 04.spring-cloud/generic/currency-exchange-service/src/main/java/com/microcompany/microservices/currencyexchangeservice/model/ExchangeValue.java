8a5ed5d31164674a08e3e0133b7f9347ecafd0c0
ricardonetmind <ricardoag@expert.netmind.es>
2024-02-02 11:13:16 +0100

    update

---
+++ 04.spring-cloud/generic/currency-exchange-service/src/main/java/com/microcompany/microservices/currencyexchangeservice/model/ExchangeValue.java
@@ -0,0 +1,34 @@
+package com.microcompany.microservices.currencyexchangeservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "Exchange_Value")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ExchangeValue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "currency_from")
+    private String from;
+
+    @Column(name = "currency_to")
+    private String to;
+
+    @Column(name = "conversion_multiple")
+    private BigDecimal conversionMultiple;
+
+    @Column
+    private int port;
+
+}
