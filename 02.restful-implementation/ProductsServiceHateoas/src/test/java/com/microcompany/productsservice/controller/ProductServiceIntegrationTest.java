782d5a6bc8922bd951ca2a5e8a669296729345e7
ricardonetmind <ricardoag@expert.netmind.es>
2024-02-10 14:18:17 +0100

    update

---
+++ 02.restful-implementation/ProductsServiceHateoas/src/test/java/com/microcompany/productsservice/controller/ProductServiceIntegrationTest.java
@@ -0,0 +1,81 @@
+package com.microcompany.productsservice.controller;
+
+import com.microcompany.productsservice.model.Product;
+import com.microcompany.productsservice.persistence.ProductsRepository;
+import com.microcompany.productsservice.service.ProductsService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+public class ProductServiceIntegrationTest {
+
+    @TestConfiguration
+    static class ProductServiceImplTestContextConfiguration {
+        @Bean
+        public ProductsService productsService() {
+            return new ProductsService();
+        }
+
+        @Bean
+        public ProductServiceController productsServiceController() {
+            return new ProductServiceController();
+        }
+    }
+
+    @BeforeEach
+    public void setUp() {
+        List<Product> products = Arrays.asList(
+                new Product(1L, "Fake product", "")
+        );
+
+        Mockito.when(productsRepository.findByNameContaining("Fake"))
+                .thenReturn(products);
+
+        Mockito.when(productsRepository.findAll())
+                .thenReturn(products);
+    }
+
+    @Autowired
+    private ProductsService productsService;
+
+    @Autowired
+    private ProductServiceController controller;
+    @MockBean
+    private ProductsRepository productsRepository;
+
+    @Test
+    public void whenValidText_thenProductsShouldBeFound() {
+        String text = "Fake";
+        List<Product> found = productsService.getProductsByText(text);
+        assertThat(found).isNotEmpty();
+        assertThat(found.get(0).getName()).contains("Fake");
+
+    }
+
+    @Test
+    void givenProducts_whengetAllProducts_thenIsNotNull() {
+        ResponseEntity<List<Product>> products = controller.getAllProducts();
+
+        assertThat(products.getStatusCode().value())
+                .isEqualTo(HttpStatus.OK.value());
+
+        assertThat(products.getBody())
+                .isNotNull();
+    }
+
+}
