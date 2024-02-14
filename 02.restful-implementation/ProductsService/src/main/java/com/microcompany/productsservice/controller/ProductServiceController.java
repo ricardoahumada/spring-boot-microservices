03c7f24e7ee3ba4b98d1c3815e6a34f12b82a4a9
Ricardo <ricardo@enmotionvalue.com>
2024-02-14 17:06:09 +0100

    update

--- 02.restful-implementation/ProductsService/src/main/java/com/microcompany/productsservice/controller/ProductServiceController.java
+++ 02.restful-implementation/ProductsService/src/main/java/com/microcompany/productsservice/controller/ProductServiceController.java
@@ -1,11 +1,14 @@
 package com.microcompany.productsservice.controller;
 
 import com.microcompany.productsservice.model.Product;
+import com.microcompany.productsservice.model.StatusMessage;
 import com.microcompany.productsservice.persistence.ProductsRepository;
 import com.microcompany.productsservice.service.ProductsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -35,6 +38,17 @@ public class ProductServiceController {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Product geOne(@PathVariable("id") Long pid) {
         return repo.findById(pid).get();
+    }
+
+    @RequestMapping(value = "/{pid}", method = RequestMethod.PUT)
+    public Product update(@PathVariable("pid") Long id, @RequestBody Product product) {
+        product.setId(id);
+        return repo.save(product);
+    }
+
+    @RequestMapping(value = "/{pid}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("pid") Long id) {
+        repo.deleteById(id);
     }
 
 }
