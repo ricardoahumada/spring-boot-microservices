74bf17f60aae63c2345b8185cdeee4e4aa0b05f2
Ricardo <ricardo@enmotionvalue.com>
2024-02-14 17:57:07 +0100

    update

--- 02.restful-implementation/ProductsService/src/main/java/com/microcompany/productsservice/controller/ProductServiceController.java
+++ 02.restful-implementation/ProductsService/src/main/java/com/microcompany/productsservice/controller/ProductServiceController.java
@@ -25,8 +25,8 @@ public class ProductServiceController {
     ProductsService service;
 
     @GetMapping(value = "")
-    public ResponseEntity<Object> getAll() {
-        List<Product> prods = service.getProductsByText("");
+    public ResponseEntity<Object> getAll(@RequestParam(required = false, defaultValue = "") String texto) {
+        List<Product> prods = service.getProductsByText(texto);
         if (prods != null && !prods.isEmpty()) return ResponseEntity.status(HttpStatus.OK).body(prods);
         else
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new StatusMessage(HttpStatus.NOT_FOUND.value(), "No hay productos"));
