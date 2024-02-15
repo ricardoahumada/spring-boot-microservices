2e3ce3f57d83e14aedf31e634a949a92be13e535
Ricardo <ricardo@enmotionvalue.com>
2024-02-15 10:53:41 +0100

    update

--- 02.restful-implementation/ProductsService/src/main/java/com/microcompany/productsservice/config/GlobalExceptionController.java
+++ 02.restful-implementation/ProductsService/src/main/java/com/microcompany/productsservice/config/GlobalExceptionController.java
@@ -2,8 +2,10 @@ package com.microcompany.productsservice.config;
 
 import com.microcompany.productsservice.exception.GeneralException;
 import com.microcompany.productsservice.exception.ProductNotfoundException;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,6 +20,11 @@ public class GlobalExceptionController {
     @ExceptionHandler(ProductNotfoundException.class)
     ResponseEntity<Object> noSuchElementExceptionHandler(ProductNotfoundException exception) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Excepcion de producto");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<Object> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException exception) {
+        return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body(exception.getMessage());
     }
 
 }
