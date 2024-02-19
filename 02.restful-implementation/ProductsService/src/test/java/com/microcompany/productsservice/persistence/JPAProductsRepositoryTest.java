94b776eee608af91e7a55def1c1383fd3fb7b17f
Ricardo <ricardo@enmotionvalue.com>
2024-02-19 15:41:59 +0100

    update

--- 02.restful-implementation/ProductsService/src/test/java/com/microcompany/productsservice/persistence/JPAProductsRepositoryTest.java
+++ 02.restful-implementation/ProductsService/src/test/java/com/microcompany/productsservice/persistence/JPAProductsRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
@@ -20,10 +21,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 // TODO: uncomment
-/*@ExtendWith(SpringExtension.class)
+@ExtendWith(SpringExtension.class)
 @DataJpaTest()
 @ComponentScan(basePackages = {"com.microcompany.productsservice.persistence"})
-@AutoConfigureTestEntityManager*/
+@AutoConfigureTestEntityManager
 class JPAProductsRepositoryTest {
 
     private static final Logger logger = LoggerFactory.getLogger(JPAProductsRepositoryTest.class);
@@ -37,19 +38,19 @@ class JPAProductsRepositoryTest {
     @Test
     void findAll() {
         // given
-        /*Product aProduct = new Product(null, "Fake Product", "123-123-1234");
+        Product aProduct = new Product(null, "Fake Product", "123-123-1234");
         entityManager.persist(aProduct);
-        entityManager.flush();*/
+        entityManager.flush();
 
         // when
-       /* List<Product> prods = jpaRepo.findAll();
-        logger.info("Prods:" + prods);*/
+        List<Product> prods = jpaRepo.findAll();
+//        logger.info("Prods:" + prods);
 
         // then
-        /*assertThat(prods.size())
+        assertThat(prods.size())
                 .isGreaterThan(0);
 
-        assertNotNull(prods);*/
+        assertNotNull(prods);
     }
 
     //    @Test
@@ -67,15 +68,15 @@ class JPAProductsRepositoryTest {
     @Test
     void save() {
         // given
-        // Product aProduct = new Product(null, "Another Fake Product", "123-123-1234");
+         Product aProduct = new Product(null, "Another Fake Product", "123-123-1234");
 
         // when
-        // jpaRepo.save(aProduct);
+         jpaRepo.save(aProduct);
 
         // System.out.println(aProduct);
 
         // then
-        // assertThat(aProduct.getId()).isGreaterThan(0);
+         assertThat(aProduct.getId()).isGreaterThan(0);
     }
 
     @Test
---
+++ 02.restful-implementation/ProductsService/src/test/resources/datos_prueba.sql
@@ -0,0 +1,6 @@
+INSERT INTO products ( name, serial) VALUES
+( 'Magazine','123-123-1234'),
+( 'Toothpaste','123-123-1234'),
+( 'Travel','123-123-1234'),
+( 'Candy','123-123-1234'),
+( 'Book','123-123-1234');
