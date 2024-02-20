730f38462d3bc5c891b024d89062909a8c58c370
Ricardo <ricardo@enmotionvalue.com>
2024-02-20 16:35:22 +0100

    update

--- 03.security/jwt/ProductsService/src/main/java/com/microcompany/productsservice/config/ApplicationSecurity.java
+++ 03.security/jwt/ProductsService/src/main/java/com/microcompany/productsservice/config/ApplicationSecurity.java
@@ -68,46 +68,6 @@ public class ApplicationSecurity {
         return authConfig.getAuthenticationManager();
     }
 
-    /*@Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-//        http.authenticationProvider(authProvider());
-
-        http.authorizeRequests()
-                .antMatchers("/auth/login",
-                        "/docs/**",
-                        "/users",
-                        "/h2-ui/**",
-                        "/configuration/ui",
-                        "/swagger-resources/**",
-                        "/configuration/security",
-                        "/swagger-ui.html",
-                        "/webjars/**"
-                ).permitAll() // HABILITAR ESPACIOS LIBRES
-//                .antMatchers("/**").permitAll() // BARRA LIBRE
-//                .antMatchers("/products/**").hasAuthority(ERole.USER.name())
-                .antMatchers(HttpMethod.GET, "/products/**").hasAnyAuthority(ERole.USER.name()) //Para acceder a productos debe ser USER
-                .antMatchers("/products/**").hasAnyAuthority(ERole.ADMIN.name()) //admin puede hacer de todo
-                .anyRequest().authenticated();
-
-        http.headers().frameOptions().sameOrigin();
-
-        http.exceptionHandling()
-                .authenticationEntryPoint(
-                        (request, response, ex) -> {
-                            response.sendError(
-                                    HttpServletResponse.SC_UNAUTHORIZED,
-                                    ex.getMessage()
-                            );
-                        }
-                );
-
-        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
-    }*/
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
