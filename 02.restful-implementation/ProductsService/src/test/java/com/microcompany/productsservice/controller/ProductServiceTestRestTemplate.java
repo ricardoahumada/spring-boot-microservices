6737530c13c2c86434dc4385f1cc7c184499747e
Ricardo <ricardo@enmotionvalue.com>
2024-02-20 09:50:21 +0100

    update

--- 02.restful-implementation/ProductsService/src/test/java/com/microcompany/productsservice/controller/ProductServiceTestRestTemplate.java
+++ 02.restful-implementation/ProductsService/src/test/java/com/microcompany/productsservice/controller/ProductServiceTestRestTemplate.java
@@ -6,20 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 // TODO: uncomment and implement methods
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
-@Sql("classpath:datos_prueba.sql")
+//@ActiveProfiles("test")
+//@Sql("classpath:datos_prueba.sql")
 public class ProductServiceTestRestTemplate {
     // @Value(value = "${local.server.port}")
     @LocalServerPort
@@ -38,6 +38,13 @@ public class ProductServiceTestRestTemplate {
 
     @Test
     public void givenUrl_whenGetProducts_thenAProductExists() throws Exception {
+        ResponseEntity<Product[]> response = restTemplate.getForEntity("http://localhost:" + port + "/api/products", Product[].class);
+        System.out.println(response.getBody());
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getHeaders().getContentType()).isEqualTo(MediaType.APPLICATION_JSON_VALUE);
+        assertThat(response.getBody())
+                .extracting(Product::getName)
+                .containsExactlyInAnyOrder("Magazine");
     }
 
     @Test
