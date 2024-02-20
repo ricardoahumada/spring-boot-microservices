b61d115d2b2140686e75a82b2b0187c0adc1ec5f
Ricardo <ricardo@enmotionvalue.com>
2024-02-20 17:38:28 +0100

    update

--- 04.spring-cloud/generic/currency-exchange-service/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port) values
-(10001,'USD', 'EUR' ,1.01, 0),
-(10002,'EUR', 'USD' ,0.99, 0),
-(10003,'GBP', 'EUR' , 1.15, 0);
