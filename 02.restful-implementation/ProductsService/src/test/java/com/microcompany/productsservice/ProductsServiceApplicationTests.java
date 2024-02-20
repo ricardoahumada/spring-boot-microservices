6d9d01665e9fa5e18c97101d4e86fc0871eb9b53
Ricardo <ricardo@enmotionvalue.com>
2024-02-20 10:49:27 +0100

    update

--- 02.restful-implementation/ProductsService/src/test/java/com/microcompany/productsservice/ProductsServiceApplicationTests.java
+++ 02.restful-implementation/ProductsService/src/test/java/com/microcompany/productsservice/ProductsServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.microcompany.productsservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProductsServiceApplicationTests {
 
 	@Test
