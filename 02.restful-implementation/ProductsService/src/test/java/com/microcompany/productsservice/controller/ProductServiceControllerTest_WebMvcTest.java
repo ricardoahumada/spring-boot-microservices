8404b5fea02774effe927b395ee5d07658b6bc2d
Ricardo <ricardo@enmotionvalue.com>
2024-02-20 09:38:14 +0100

    update

--- 02.restful-implementation/ProductsService/src/test/java/com/microcompany/productsservice/controller/ProductServiceControllerTest_WebMvcTest.java
+++ 02.restful-implementation/ProductsService/src/test/java/com/microcompany/productsservice/controller/ProductServiceControllerTest_WebMvcTest.java
@@ -9,9 +9,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
--- 02.restful-implementation/ProductsService/src/test/java/com/microcompany/productsservice/controller/ProductServiceTestRestTemplate.java
+++ 02.restful-implementation/ProductsService/src/test/java/com/microcompany/productsservice/controller/ProductServiceTestRestTemplate.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.*;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.net.URI;
@@ -17,7 +18,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 // TODO: uncomment and implement methods
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@Sql("classpath:data.sql")
+@ActiveProfiles("test")
+@Sql("classpath:datos_prueba.sql")
 public class ProductServiceTestRestTemplate {
     // @Value(value = "${local.server.port}")
     @LocalServerPort
@@ -27,6 +29,11 @@ public class ProductServiceTestRestTemplate {
 
     @Test
     public void givenUrl_whenGetProducts_thenAStringExists() throws Exception {
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/api/products", String.class);
+        System.out.println(response.getBody());
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).contains("Magazine");
+        assertThat(response.getHeaders().getContentType()).isEqualTo(MediaType.APPLICATION_JSON_VALUE);
     }
 
     @Test
