7a86b05d44c069731a25f9e2eb250aed4b08222d
ricardoag <ricardo@enmotionvalue.com>
2024-04-25 16:39:36 +0200

    added multimodule

--- 02.restful-implementation/ProductsService/src/main/java/com/microcompany/productsservice/exception/GeneralException.java
+++
@@ -1,9 +0,0 @@
-package com.microcompany.productsservice.exception;
-
-public class GeneralException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public GeneralException(String message) {
-        super(message);
-    }
-}
