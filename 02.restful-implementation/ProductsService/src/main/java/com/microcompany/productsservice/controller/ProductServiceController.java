4cd199611a9fef31fa3826a24500ed04107b0aec
ricardo@enmotionvalue.com <ricardo.ahumada>
2024-05-17 10:58:02 +0200

    update

--- 02.restful-implementation/ProductsService/src/main/java/com/microcompany/productsservice/controller/ProductServiceController.java
+++ 02.restful-implementation/ProductsService/src/main/java/com/microcompany/productsservice/controller/ProductServiceController.java
@@ -2,9 +2,19 @@ package com.microcompany.productsservice.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/products")
 public class ProductServiceController {
     private static final Logger logger = LoggerFactory.getLogger(ProductServiceController.class);
 
+
+    @RequestMapping("")
+    public String info() {
+        logger.info("Prouctos.info ejecutándose .......");
+        return "Productos";
+    }
 
 }
