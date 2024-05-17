25a488f0deefc35c1c9f9276ea6b8f0a3804219a
ricardo@enmotionvalue.com <ricardo.ahumada>
2024-05-17 11:30:02 +0200

    update

--- 02.restful-implementation/ProductsService/src/main/java/com/microcompany/productsservice/controller/ProductServiceController.java
+++ 02.restful-implementation/ProductsService/src/main/java/com/microcompany/productsservice/controller/ProductServiceController.java
@@ -1,12 +1,15 @@
 package com.microcompany.productsservice.controller;
 
 import com.microcompany.productsservice.model.Product;
+import com.microcompany.productsservice.model.StatusMessage;
 import com.microcompany.productsservice.persistence.IProductsRepository;
 import com.microcompany.productsservice.persistence.ProductsRepository;
 import com.microcompany.productsservice.service.ProductsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,12 +35,14 @@ public class ProductServiceController {
     }*/
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public List<Product> getAllProducts() {
-        return service.getProductsByText("");
+    public ResponseEntity getAllProducts() {
+        List<Product> prods = service.getProductsByText("");
+        if (prods != null && prods.size() > 0) return ResponseEntity.status(HttpStatus.OK).body(prods);
+        else return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new StatusMessage(HttpStatus.OK.value(), "No hay productos"));
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
-    public Product add(@RequestBody Product newP){
+    public Product add(@RequestBody Product newP) {
         return repo.save(newP);
     }
 
