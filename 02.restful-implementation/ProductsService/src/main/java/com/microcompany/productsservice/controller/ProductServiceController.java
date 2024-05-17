28c5648b8018c8f9a21dcf820efa2b6bc633070a
ricardo@enmotionvalue.com <ricardo.ahumada>
2024-05-17 11:19:45 +0200

    update

--- 02.restful-implementation/ProductsService/src/main/java/com/microcompany/productsservice/controller/ProductServiceController.java
+++ 02.restful-implementation/ProductsService/src/main/java/com/microcompany/productsservice/controller/ProductServiceController.java
@@ -2,10 +2,12 @@ package com.microcompany.productsservice.controller;
 
 import com.microcompany.productsservice.model.Product;
 import com.microcompany.productsservice.persistence.IProductsRepository;
+import com.microcompany.productsservice.persistence.ProductsRepository;
 import com.microcompany.productsservice.service.ProductsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +19,11 @@ import java.util.List;
 public class ProductServiceController {
     private static final Logger logger = LoggerFactory.getLogger(ProductServiceController.class);
 
-
     @Autowired
     ProductsService service;
+
+    @Autowired
+    private ProductsRepository repo;
 
    /* @RequestMapping("")
     public String info() {
@@ -30,6 +34,11 @@ public class ProductServiceController {
     @RequestMapping(value = "", method = RequestMethod.GET)
     public List<Product> getAllProducts() {
         return service.getProductsByText("");
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public Product add(@RequestBody Product newP){
+        return repo.save(newP);
     }
 
 }
--- 02.restful-implementation/ProductsService/src/main/resources/data.sql
+++ 02.restful-implementation/ProductsService/src/main/resources/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO products (id, name) VALUES
-(1, 'Magazine'),
-(2, 'Toothpaste'),
-(3, 'Travel'),
-(4, 'Candy'),
-(5, 'Book');
+INSERT INTO products (name) VALUES
+('Magazine'),
+('Toothpaste'),
+('Travel'),
+('Candy'),
+('Book');
 
 -- INSERT INTO users (id, email, password) VALUES
 -- (1, 'r@r.com','mypass');
