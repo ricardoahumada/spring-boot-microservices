d7ad58974094f1c12130f52042ea6dacc28f27f8
ricardo@enmotionvalue.com <ricardo.ahumada>
2024-05-22 12:45:06 +0200

    update

--- 02.restful-implementation/AccountsService/src/main/java/com/microcompany/accountsservice/config/GlobalExceptionController.java
+++ 02.restful-implementation/AccountsService/src/main/java/com/microcompany/accountsservice/config/GlobalExceptionController.java
@@ -4,8 +4,15 @@ import com.microcompany.accountsservice.exception.AccountNotfoundException;
 import com.microcompany.accountsservice.exception.GlobalException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionController {
@@ -15,10 +22,27 @@ public class GlobalExceptionController {
         return new ResponseEntity<>("GlobalException:"+exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-    /*@ExceptionHandler(value = AccountNotfoundException.class)
+    @ExceptionHandler(value = AccountNotfoundException.class)
     public ResponseEntity<Object> exception(AccountNotfoundException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
-    }*/
+    }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
+        return new ResponseEntity<>("not valid due to validation error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 
 }
